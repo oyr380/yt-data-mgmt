edd06eff1ee44aff925ede1f93f6c3afcc1c2765
oyr380 <oyr380@my.utsa.edu>
2022-04-23 15:12:20 -0500

    Reworked script to go channel by channel, video by video. Added loading icon and options for quiet output

--- scripts/metadata_downloader.py
+++ scripts/metadata_downloader.py
@@ -42,11 +42,27 @@ archive_path = os.path.normpath("archive.txt")
 
 
 # Batch file containing channel URLs
-batch_file = os.path.normpath("batch_vids.txt")
+batch_path = os.path.normpath("batch_vids.txt")
 
 # ===============================================================
 
 
+#For loading symbol
+def spinning_cursor():
+    while True:
+        for cursor in "|/-\\":
+            yield cursor
+
+#Attempt at loading progress bar as it's longer
+def progress_bar(max_length=10, char='#'):
+
+    bars = []
+    for i in range(1, max_length + 1):
+        bars.append(''.join([x*i for x in char]))
+
+    while True:
+        for cursor in bars:
+            yield cursor
 
 def ytdlp_get_channel_name(channel_url, seconds=5):
     channel_name = ''
@@ -141,7 +157,65 @@ def ytdlp_get_ids(channel_url):
     output.stdout.flush()
     return video_ids
 
+def ytdlp_download_videos(videos, progress=False, quiet=False):
 
+    num_downloads = 0
+
+    for video in videos:
+        if progress is True:
+            print("Video {}/{}".format(num_downloads + 1, len(videos)))
+        # If video is successfully downloaded, count it
+        if ytdlp_download_video(video, quiet) == 0:
+            num_downloads += 1
+
+    return num_downloads
+
+def ytdlp_download_video(video, quiet=False):
+    args = ytdlp_args[:]
+    if quiet is True:
+        args.append("--quiet")
+    args.append("https://www.youtube.com/watch?v=" + video)
+
+    spinner = spinning_cursor()
+    num_cursors = 10
+
+    # Run yt-dlp to download video metadata
+    output = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    # Print and parse the output line by line as yt-dlp runs
+    if quiet is False:
+        for stdout_line in iter(output.stdout.readline, ""):
+            #print(len(stdout_line))
+            if len(stdout_line) > 0 and quiet is False:
+                print(stdout_line[:-1])
+
+            else:
+                break
+
+
+    # Wait for yt-dlp to finish running to get return code
+    while output.poll() is None:
+        # Print loading icon so you know it's working
+        # If this stops spinning, something broke
+        #sys.stdout.write(next(spinner))
+        progress = next(spinner)
+        for i in range(num_cursors):
+            sys.stdout.write(progress)
+        sys.stdout.flush()
+        time.sleep(0.5)
+        for i in range(num_cursors):
+            sys.stdout.write('\b')
+        #time.sleep(0.5)
+
+    # If return code is 0 (success) then append video ID to archive file
+    if output.returncode == 0:
+        append_file_list(archive_path, ["youtube " + video])
+
+    # TODO
+    # add else to throw the video non-working ID into a log file for reference later on
+    # Don't expect this to be very necessary
+    #
+    return output.returncode
 
 def write_file_list(path, write_list, filename=None):
     '''
@@ -260,9 +334,8 @@ if __name__ == '__main__':
     archive_dict = dict([(value, key) for key, value in archive_dict.items()])
 
 
-    # Assumes there is a file called "batch_vids.txt"
     # Reads all of the channel URLs into the channels[] list
-    with open(batch_file, 'r') as batch_file:
+    with open(os.path.join(batch_path), 'r') as batch_file:
         lines = batch_file.readlines()
         for line in lines:
             #If the URL doesn't end in a / add one
@@ -280,7 +353,8 @@ if __name__ == '__main__':
     # List of completed channel URLs
     completed_channels = []
 
-    # Go through each channel URL
+    # Go through each channel URL and see if it's been downloaded before
+    # Download undownloaded videos
     for channel_url in channels:
         video_ids = []
 
@@ -292,89 +366,56 @@ if __name__ == '__main__':
             if video not in archive_dict:
                 new_downloads.append(video)
 
-        #If there are no new_downloads (videos) then channel marked as complete
-        if len(new_downloads) == 0:
-            #completed_channels.append(channel_url + '\n')
-            completed_channels.append(channel_url)
+        print("Downloading video info from: {}".format(ytdlp_get_channel_name(channel_url)))
+        num_downloads = ytdlp_download_videos(list(dict.fromkeys(new_downloads)), True, True)
+        #If there are no new_downloads (videos) OR all videos were downloaded, then mark ias compelted
+        if len(new_downloads) == 0 or num_downloads == len(new_downloads):
 
-        # Go through each new video ID and download the data
-        # Updates the archive file after successful download
-        for video in list(dict.fromkeys(new_downloads)):
-            #NOTE - Swapped out relying on the config for just using the commands in the config
-            #args = ytdlp_download.split(' ')
-            #args.append(config_path)
-            args = ytdlp_args[:]
-            args.append("https://www.youtube.com/watch?v=" + video)
+            #Remove from batch file and append to completed file
+            with open(os.path.join(batch_path), 'r+') as fp:
+                #Append to completed file
+                append_file_list(os.getcwd(), completed_channels, 'completed.txt')
 
-            print(args)
+                lines = fp.readlines()
+                lines.remove(channel_url)
+                fp.writelines(lines)
 
-            # Run yt-dlp to download video metadata
-            # FIXME temp
-            # output = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            # # Print and parse the output line by line as yt-dlp runs
-            # for stdout_line in iter(output.stdout.readline, ""):
-            #     #print(len(stdout_line))
-            #     if len(stdout_line) > 0:
-            #         print(stdout_line[:-1])
-            #     else:
-            #         break
-            # # Wait for yt-dlp to finish running to get return code
-            # while output.poll() is None:
-            #     time.sleep(0.5)
 
-            # If return code is 0 (success) then append video ID to archive file
-            # if output.returncode == 0:
-            if True is True:
-                with open(os.path.join(archive_path), 'a') as fp:
-                    newline = "youtube " + video
-                    #fp.write('\n')
-                    fp.write(newline)
-                    fp.write('\n')
-
-            # TODO
-            # add else to throw the video non-working ID into a log file for reference later on
-            # Don't expect this to be very necessary
 
 
 
     # FIXME - Can add harmless duplicates to file
     # Solution may be best in another script to replicate "sort -u" capability
-    # Add completed channel to completed.txt
-    # with open('completed.txt', 'a+') as fp:
-    #     fp.writelines(completed_channels)
-    append_file_list(os.getcwd(), completed_channels, 'completed.txt')
+    # with open('batch_vids.txt', 'r+') as fp:
+    #     lines = fp.readlines()
 
-    # Remove completed channel from batch_vids.txt
-    with open('batch_vids.txt', 'r+') as fp:
-        lines = fp.readlines()
+    #     index = 0
+    #     #print(lines)
+    #     for line in lines:
+    #         #If the URL doesn't end in a / add one
+    #         #Assumes specific format currently adhered to in current list
+    #         if line[-2] != "/":
+    #             line = line.rstrip('\n') + '/'
+    #         lines[index] = line
+    #         index += 1
+    #     for channel in completed_channels:
+    #         #if channel[:-1] in lines:
+    #         # Was accounting for the trailing slash or not but 2nd if should be redundant now
+    #         if channel in lines:
+    #             print("COMPLETED: " + channel)
+    #             lines.remove(channel)
+    #         if (channel[:-2] + '\n') in lines:
+    #             print("COMPLETED: " + channel)
+    #             lines.remove(channel[:-2] + '\n')
 
-        index = 0
-        #print(lines)
-        for line in lines:
-            #If the URL doesn't end in a / add one
-            #Assumes specific format currently adhered to in current list
-            if line[-2] != "/":
-                line = line.rstrip('\n') + '/'
-            lines[index] = line
-            index += 1
-        for channel in completed_channels:
-            #if channel[:-1] in lines:
-            # Was accounting for the trailing slash or not but 2nd if should be redundant now
-            if channel in lines:
-                print("COMPLETED: " + channel)
-                lines.remove(channel)
-            if (channel[:-2] + '\n') in lines:
-                print("COMPLETED: " + channel)
-                lines.remove(channel[:-2] + '\n')
+    #     # Point back to beginning of file before writing again
+    #     # Otherwise it'll just dupe some or all of the file
+    #     fp.seek(0, 0)
 
-        # Point back to beginning of file before writing again
-        # Otherwise it'll just dupe some or all of the file
-        fp.seek(0, 0)
-
-        # If there are no more urls in file, write empty file
-        if len(lines) == 0:
-            with open('batch_vids.txt', 'w') as fp:
-                pass
-        # Write remaining lines to file
-        else:
-            fp.writelines(lines)
+    #     # If there are no more urls in file, write empty file
+    #     if len(lines) == 0:
+    #         with open('batch_vids.txt', 'w') as fp:
+    #             pass
+    #     # Write remaining lines to file
+    #     else:
+    #         fp.writelines(lines)
