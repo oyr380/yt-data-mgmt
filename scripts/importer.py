0036a02868f3a3839ac1f9fcfd8e012bb726f74e
oyr380 <oyr380@my.utsa.edu>
2022-05-03 01:09:21 -0500

    Added basic progress counter

--- scripts/importer.py
+++ scripts/importer.py
@@ -28,6 +28,7 @@ def mongo_connect(URI):
         print("ERROR: Could not connect to database.")
         sys.exit(1)
 
+    print("Connected to database.")
     return client
 
 def check_jsons_complete(jsons):
@@ -73,45 +74,50 @@ if __name__ == '__main__':
     clean_jsons = jsons.clean_files
     info_jsons = jsons.info_files
 
-    count = 0
+    upload_count = 0
 
+    counter = 0
 
+    total = len(clean_jsons)
 
     for json_path in clean_jsons:
         val = JSONParser.get_json_value_from_path(json_path, 'id')
+        counter += 1
 
-        #If this is a video json
-        #Video IDs are 11 characters long
-        if len(val) == 11:
-            video_title = JSONParser.get_json_value_from_path(json_path, 'title')
-            # Upload json to videos collection if video not found
-            # If video is not already in collection, try to import it
-            if val not in vid_dict:
-                with open(json_path, 'r') as fp:
-                    json_obj = json.load(fp)
-                    try:
-                        print("Importing {} into {} collection.".format(video_title, 'videos'))
-                        video_collection.insert_one(json_obj)
-                    except:
-                        print("{} failed to import into {} collection.".format(video_title, 'videos'))
-                print("---------")
-                count +=1
+        if len(val) == 11 or len(val == 24):
+            print("File Progress - {}/{}".format(counter, total))
+            #If this is a video json
+            #Video IDs are 11 characters long
+            if len(val) == 11:
+                video_title = JSONParser.get_json_value_from_path(json_path, 'title')
+                # Upload json to videos collection if video not found
+                # If video is not already in collection, try to import it
+                if val not in vid_dict:
+                    with open(json_path, 'r') as fp:
+                        json_obj = json.load(fp)
+                        try:
+                            print("Importing {} into {} collection.".format(video_title, 'videos'))
+                            video_collection.insert_one(json_obj)
+                        except:
+                            print("{} failed to import into {} collection.".format(video_title, 'videos'))
+                    print("---------")
+                    upload_count +=1
 
-        # If this is a channel json
-        elif len(val) == 24:
-            uploader = JSONParser.get_json_value_from_path(json_path, 'uploader')
+            # If this is a channel json
+            elif len(val) == 24:
+                uploader = JSONParser.get_json_value_from_path(json_path, 'uploader')
 
-            if val not in channel_dict:
-                with open(json_path, 'r') as fp:
-                    json_obj = json.load(fp)
-                    try:
-                        print("Importing {} into {} collection.".format(uploader, 'channels'))
-                        channel_collection.insert_one(json_obj)
-                    except:
-                        print("{} failed to import into {} collection.".format(uploader, 'channels'))
-                print("---------")
-                sys.exit()
-                count +=1
+                if val not in channel_dict:
+                    with open(json_path, 'r') as fp:
+                        json_obj = json.load(fp)
+                        try:
+                            print("Importing {} into {} collection.".format(uploader, 'channels'))
+                            channel_collection.insert_one(json_obj)
+                        except:
+                            print("{} failed to import into {} collection.".format(uploader, 'channels'))
+                    print("---------")
+                    sys.exit()
+                    upload_count +=1
 
 
-    print("Uploaded {} documents".format(count))
+    print("Uploaded {} documents".format(upload_count))
